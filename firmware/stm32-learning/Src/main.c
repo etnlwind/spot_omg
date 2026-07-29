826fff4f8a540f7ef72513a0cdd3a344e5d41faa
Sangjin RYU <etnlwind@gmail.com>
2026-07-29 13:21:24 +0900

    feat: 버튼 입력에 따른 LED 출력 기능 추가
    - PC13 GPIO 입력 설정
    - PA5 GPIO LED 출력 제어
    - 액티브 로우 버튼 상태에 따라 LED 점등

--- firmware/stm32-learning/Src/main.c
+++ firmware/stm32-learning/Src/main.c
@@ -95,16 +95,18 @@ int main(void)
 
   /* Infinite loop */
   /* USER CODE BEGIN WHILE */
-  char message[] = "Hello STM32\r\n";
-
   while (1)
   {
-    HAL_UART_Transmit(&huart2,
-                      (uint8_t *)message,
-                      sizeof(message) - 1,
-                      HAL_MAX_DELAY);
 
-    HAL_Delay(1000);
+    if (HAL_GPIO_ReadPin(GPIOC, GPIO_PIN_13) == GPIO_PIN_RESET)
+    {
+      HAL_GPIO_WritePin(GPIOA, GPIO_PIN_5, GPIO_PIN_SET);
+    }
+    else
+    {
+      HAL_GPIO_WritePin(GPIOA, GPIO_PIN_5, GPIO_PIN_RESET);
+    }
+
   }
   /* USER CODE END 3 */
 }
@@ -196,10 +198,17 @@ static void MX_GPIO_Init(void)
   /* USER CODE END MX_GPIO_Init_1 */
 
   /* GPIO Ports Clock Enable */
+  __HAL_RCC_GPIOC_CLK_ENABLE();
   __HAL_RCC_GPIOA_CLK_ENABLE();
 
   /*Configure GPIO pin Output Level */
   HAL_GPIO_WritePin(GPIOA, GPIO_PIN_5, GPIO_PIN_RESET);
+
+  /*Configure GPIO pin : PC13 */
+  GPIO_InitStruct.Pin = GPIO_PIN_13;
+  GPIO_InitStruct.Mode = GPIO_MODE_INPUT;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  HAL_GPIO_Init(GPIOC, &GPIO_InitStruct);
 
   /*Configure GPIO pin : PA5 */
   GPIO_InitStruct.Pin = GPIO_PIN_5;
