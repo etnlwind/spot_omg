548ffc7af604ba09d1b48ceb0fbd77e6452ea21d
Sangjin RYU <etnlwind@gmail.com>
2026-07-29 12:42:49 +0900

    feat: Lesson 01 GPIO LED 점멸 구현 완료
    - PA5 GPIO 출력 토글 방식 적용
    - 500ms 간격 LED 점멸 구현

--- firmware/lesson-01-led/Src/main.c
+++ firmware/lesson-01-led/Src/main.c
@@ -92,18 +92,20 @@ int main(void)
 
   /* Infinite loop */
   /* USER CODE BEGIN WHILE */
-  while (1)
 
+  while (1)
+  {
+      HAL_GPIO_TogglePin(GPIOA, GPIO_PIN_5);
+      HAL_Delay(500);
+  }
+
+  while (1)
   {
 
     HAL_GPIO_WritePin(GPIOA, GPIO_PIN_5, GPIO_PIN_SET);
-
     HAL_Delay(500);
-
     HAL_GPIO_WritePin(GPIOA, GPIO_PIN_5, GPIO_PIN_RESET);
-
     HAL_Delay(500);
-
   }
   /* USER CODE END 3 */
 }
